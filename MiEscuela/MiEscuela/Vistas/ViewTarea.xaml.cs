50bf9410d0454fa092f05c8d8ef0494ec6c7813e
Gerardo Nava <gerardonavart@outlook.com>
2018-06-06 04:14:16 -0500

    Se añade la vista ViewTarea

--- MiEscuela/MiEscuela/Vistas/ViewTarea.xaml.cs
+++ MiEscuela/MiEscuela/Vistas/ViewTarea.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MiEscuela.COMMON.Entidades;
+using MiEscuela.COMMON.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,49 @@ namespace MiEscuela.Vistas
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ViewTarea : ContentPage
 	{
-		public ViewTarea ()
+        Tarea tarea;
+        ITareaManager tareaManager;
+        bool nuevo;
+		public ViewTarea (Tarea t, bool n)
 		{
+            tarea = t;
+            nuevo = n;
 			InitializeComponent ();
+
+            btnGuardar.Clicked += (sender, e) =>
+            {
+                if (nuevo)
+                {
+                    tarea = tareaManager.Agregar(contenedor.BindingContext as Tarea);
+                }
+                else
+                {
+                    tarea = tareaManager.Modificar(contenedor.BindingContext as Tarea);
+                }
+                if (tarea != null)
+                {
+                    DisplayAlert("Mi Escuela", "Materia Guardada", "OK");
+                    Navigation.PopAsync();
+                }
+                else
+                {
+                    DisplayAlert("Mi Escuela", "Error al Guardar", "OK");
+                }
+            };
+
+            btnEliminar.Clicked += (sender, e) =>
+             {
+                 if (tareaManager.Eliminar(tarea.Id))
+                 {
+                     DisplayAlert("Mi Escuela", "Materia Eliminada Correctamente", "Ok");
+                     Navigation.PopAsync();
+                 }
+                 else
+                 {
+                     DisplayAlert("Mi Escuela", "No se pudo eliminar la materia", "Ok");
+                 }
+             };
+
 		}
 	}
 }
